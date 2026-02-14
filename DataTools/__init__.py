5847e4a4e3193ffd9309d7b17ea91b6dcbadb073
Elishah John <elishahjohn9@gmail.com>
2026-02-14 09:08:09 -0800

    replace_nan function

--- DataTools/__init__.py
+++ DataTools/__init__.py
@@ -1,3 +1,3 @@
-from .cleaning import missing_values, drop_col, drop_row, to_datetime, to_numeric, clean_dataframe
+from .cleaning import missing_values, drop_col, drop_row, to_datetime, to_numeric, clean_dataframe, replace_nan
 from .exploration import summarize_df, null_columns, overview
 from .visuals import visual_relation, correlation_matrix, data_distribution
